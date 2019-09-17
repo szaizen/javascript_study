9d7fbca5bbf3eab009d80997ce5dfac43508f6e1
saki <zaizen.saki@gmail.com>
2019-09-17 16:35:45 +0900

    問題53

--- study2.js
+++ study2.js
@@ -21,8 +21,23 @@ for(var entry of entries) {
 /*
 52.'morita kenji'のような1つ以上の小英字、半角スペース、1つ以上の小英字にマッチした場合、
 配列['morita kenji']が返るようにしてください。
-
 */
 
 /\w+\s\w+/.exec('morita kenji');
+
+/*
+53. このような 'It is an important problem'と'The import duty is not cheap'の
+文字列内importにマッチするとbooleanを返す記述をしてください
+*/
+
+const str = 'It is an important problem';
+const str2 = 'The import duty is not cheap';
+
+function textCheck(text) {
+	return /.*\bimport\b.*/.test(text);
+}
+
+textCheck(str); // false
+textCheck(str2); // true
+
 
