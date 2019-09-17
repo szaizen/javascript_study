f3c59720fcf74aae2286d9bbdc4f9c01da24da83
saki <zaizen.saki@gmail.com>
2019-09-17 16:19:46 +0900

    問題51

---
+++ study2.js
@@ -0,0 +1,20 @@
+/*-------------------------------------------
+*
+*  JavaScript練習問題集
+*  https://gist.github.com/kenmori/1961ce0140dc3307a0e641c8dde6701d
+*
+*  51問目から
+*
+*-------------------------------------------*/
+
+/*
+51.問50の変数fafa内にある要素を出力してください 
+//期待する出力 //['one','info@fa'] //['two', 'send@fafa'] //['three', 'hoso@fafa']
+*/
+
+var entries = map.entries();
+
+for(var entry of entries) {
+	console.log(entry);
+}
+
